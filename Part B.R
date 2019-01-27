d778c99df073d137c1251b5b6203a9d32d897dcd
Giuseppe Calabrese <calabrese.giuseppe94@gmail.com>
2019-01-27 17:57:08 +0100

    part B .R file updated

--- Part B.R
+++ Part B.R
@@ -1,11 +1,16 @@
 df <- read.csv("/home/data/MScDS/SDS/SDSHW3/NGC6611.csv")
 
-hist(df$Mass, col = 'orchid', border = 'white', freq = F)
+hist(df$Mass, col = 'orchid', border = 'white')
+"The histogram of the masses recorded in the NGC6611 dataset shows that 
+the most common mass value is under 0.5. Moreover, as the value of the mass
+increases, we get an increasingly lower number of observation for that value."
 
-d <- density(df[,'Mass'])
+d <- density(df$Mass)
 plot(d, col = 'purple')
-b <- boxplot(df[, 'Mass'])
+"The density plot shows a multimodal distribution even though the distribution
+is quite a decreasing distribution after the first mode."
 
-# The shape of the mass distribution is a multimodal shape.
-# Put tante boiate here.
-
+b <- boxplot(df$Mass)
+b$stats
+"Now we see that most of the data is between 0.1575 and 0.6715 and few of them
+is above 0.6715"
